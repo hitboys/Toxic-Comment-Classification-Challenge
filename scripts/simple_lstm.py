225fe3070f74e8f1aa8db925063f932a99e026b5
hitzzc <hitzzc@126.com>
2018-02-01 17:41:54 +0800

    remove unused import

--- scripts/simple_lstm.py
+++ scripts/simple_lstm.py
@@ -26,7 +26,6 @@ from gensim.models import KeyedVectors
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.layers import Dense, Input, LSTM, Embedding, Dropout, Activation
-from keras.layers.merge import concatenate
 from keras.models import Model
 from keras.layers.normalization import BatchNormalization
 from keras.callbacks import EarlyStopping, ModelCheckpoint
